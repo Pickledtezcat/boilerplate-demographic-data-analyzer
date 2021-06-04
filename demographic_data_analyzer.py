d7d7291864c9c192b0363bb99bd167532e598070
Pickledtezcat <>
2021-06-04 04:16:19 +0000

    processed to "highest earning country"

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -5,39 +5,57 @@ def calculate_demographic_data(print_data=True):
     # Read data from file
     df = pd.read_csv("adult.data.csv") 
 
+    people = df.shape[0]
+
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
 
-    #print(df["sex"].head())
+    print(df.columns)
+    print(df["salary"].tail())
 
     race_count = df["race"].value_counts()
 
     # What is the average age of men?
 
     men_mask = df["sex"] == "Male" 
-    average_age_men = df[men_mask].mean()
-    print(average_age_men)
+    average_age_men = round(df[men_mask]["age"].mean(), 1)
+    #print(average_age_men)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+
+    bat_mask = df["education"] == "Bachelors"
+    percentage_bachelors = round(df[bat_mask].shape[0] / people * 100.0, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
-    # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    ed_mask = df["education"].isin(["Bachelors", "Masters", "Doctorate"])
+
+    educated = df[ed_mask]
+    uneducated = df[~ed_mask]
+    
+    sal_mask = df["salary"] == ">50K"
+
+    educated_high_earners = df[(ed_mask & sal_mask)]    
+    uneducated_high_earners = df[(~ed_mask & sal_mask)]
+
+    # with and without `Bachelors`, percentr `Doctorate`
+    higher_education = df[ed_mask]
+    lower_education = df[~ed_mask]
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = round((educated_high_earners.shape[0] / educated.shape[0]) * 100.0, 1)
+    lower_education_rich = round((uneducated_high_earners.shape[0] / uneducated.shape[0]) * 100.0, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
-
+    min_work_hours = df["hours-per-week"].min()
+    
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    num_min_workers = df[df["hours-per-week"] == 1]
 
-    rich_percentage = None
+    rich_min_worker_mask = (df["hours-per-week"] == 1) & (df["salary"] == ">50K")
+    rich_percentage = round(df[rich_min_worker_mask].shape[0] / df[df["hours-per-week"] == 1].shape[0] * 100.0)
+
+    print (rich_percentage)
 
     # What country has the highest percentage of people that earn >50K?
     highest_earning_country = None
